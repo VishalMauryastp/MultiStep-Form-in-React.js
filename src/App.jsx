633c99df8052647821c1902cfa0d610a5926cbde
Vishal Maurya <vishalmaurya12nov@gmil.com>
2024-03-15 17:00:41 +0530

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Booking />
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Booking />} />
-    //     <Route path="/booking" element={<MultiStepForm />} />
-    //   </Routes>
-    // </BrowserRouter>
+    // <Booking />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Booking />} />
+        <Route path="/booking" element={<MultiStepForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
